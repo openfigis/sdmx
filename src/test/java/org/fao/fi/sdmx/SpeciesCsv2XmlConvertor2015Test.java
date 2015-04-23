958a8d853bc788a473f120071c0d719d726a55ce
erikvaningen <erikvaningen@gmail.com>
2015-04-23 11:54:02 +0200

    preparing the 2015 version

---
+++ src/test/java/org/fao/fi/sdmx/SpeciesCsv2XmlConvertor2015Test.java
@@ -0,0 +1,14 @@
+package org.fao.fi.sdmx;
+
+import org.junit.Test;
+
+public class SpeciesCsv2XmlConvertor2015Test {
+
+	SpeciesCsv2XmlConvertor2015 s = new SpeciesCsv2XmlConvertor2015();
+
+	@Test
+	public void testProcess() {
+		s.process();
+	}
+
+}
